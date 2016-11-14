10e7037b8f011c40c01cc59fd4ee726e10a35e74
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-11-14 09:17:15 -0800

    board and clicks working

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
 $(document).ready(function() {
- 
+  var counter = 0;
+  var X = "X";
+  var O = "O";
+  var togglePlayer = X;
+  var previouslySelected = {
+  X:[],
+  O:[]
+  };
   var board = new Array(3);
   board[0] = new Array(3);
   board[1] = new Array(3);
   board[2] = new Array(3);
 
+  function toggleTurn() {
+    if (togglePlayer == X) {
+      togglePlayer = O;
+    } else {
+      togglePlayer = X;
+    }
+  };
+
+  // function hasBeenClicked() {
+  //   if (($(this).find('.player')[0])) {
+  //     alert("THIS BOX HAS BEEN SELECTED! PICK ANOTHER BOX");
+  //   } else {
+  //     $(this).append("<h2 class='player'>" + togglePlayer + "</h2>");
+  //     console.log($(this).find('.player')[0].innerText);
+  //      counter += 1;
+  //     }
+  // };
+  function checkColWinner(){
+    var x = 0;
+    var o = 0;
+    for (let col = 0; col < 3; col++) {
+      for(let row = 0; row < 3; row++) {
+        if (false) {};
+      };
+    };
+  };
+  function isGameOver(counter) {
+    if (counter >= 9) {
+      alert("TIE! NO WINNER.");
+    }
+  };
+
   $('.box').click(function() {
     var col = $(this).attr('class').slice(-1);
     var row = $(this).parent().attr('class').slice(-1);
+    var box = [col, row];
 
-    console.log("Row: " + row + " Col: " + col);
-    // console.log($(this).attr('class'));
-    // console.log($(this).parent());
-  })
+    if (($(this).find('.player')[0])) { // check if box has child of class player (i.e. if its been clicked)
+      alert("THIS BOX HAS BEEN SELECTED! PICK ANOTHER BOX");
+    } else {
+      $(this).append("<h2 class='player'>" + togglePlayer + "</h2>");
+      console.log($(this).find('.player')[0].innerText);
+       counter += 1;
+    };
+    // hasBeenClicked();
+    // console.log(col);
+    // console.log(row);
+    toggleTurn(); // works
+    isWinner();
+    isGameOver(counter);
+  });
 });
